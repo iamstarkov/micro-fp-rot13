91a0481db36d3f2b45d3d7b8fe3b60cf1ac123e7
Vladimir Starkov <iamstarkov@gmail.com>
2017-03-29 23:23:55 +0200

    wip++

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const url = require('url');
 const R = require('ramda');
 const rot13 = require('rot-13');
 
-// match :: RegExp -> Request -> _Response -> Route
-const match = R.curry((route, req, _res) => route.test(req.url));
+// match :: RegExp -> Request -> Route
+const match = R.curry((route, req) => route.test(req.url));
 
 // urlParse :: String -> Object
 const urlParse = x => url.parse(x, true);
 
-// rot13Route :: Request -> Response -> String
-const rot13Route = R.curry((req, _res) => R.pipe(
+// rot13Route :: Request -> String
+const rot13Route = R.pipe(
   R.prop('url'),
   urlParse,
   R.path(['query', 'rot13']),
   rot13
-)(req));
+);
 
 module.exports = R.cond([
 
